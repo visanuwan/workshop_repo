352d8aca3acbb169845ce2e9ef8ac9095c9b0546
Visanu Wanchai <visanuw86@gmail.com>
2016-04-19 00:59:53 +0200

    Adding sudo through shell script

--- ini_env.sh
+++ ini_env.sh
@@ -8,7 +8,7 @@ then
 	echo "setxkbmap -option grp:switch,grp:alt_shift_toggle us,no" >> $bashrc
 fi
 
-sed -i '/\#sudo mount -t vboxsf -o uid=1000,gid=1000 Vboxshare \/home\/visanuwan\/VboxShare/c\sudo mount -t vboxsf -o uid=1000,gid=1000 Vboxshare \/home\/visanuwan\/VboxShare' $rc_local
+sudo sed -i '/\#sudo mount -t vboxsf -o uid=1000,gid=1000 Vboxshare \/home\/visanuwan\/VboxShare/c\sudo mount -t vboxsf -o uid=1000,gid=1000 Vboxshare \/home\/visanuwan\/VboxShare' $rc_local
 
 if ! grep -Fxq "export PATH=/home/visanuwan/AssemblyTools/FastQC:/home/visanuwan/AssemblyTools/SPAdes-3.7.1-Linux/bin:/home/visanuwan/AssemblyTools/quast-3.2:\$PATH" $bashrc
 then
